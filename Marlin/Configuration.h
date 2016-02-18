ce48758449076c1c3fce68a96600bdeb77374c35
kitprinter3d <kitprinter3d@users.noreply.github.com>
2016-02-18 11:19:32 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -758,10 +758,10 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 //#define U8GLIB_SH1106
 
 
-//#define SAV_3DGLCD
+#define SAV_3DGLCD
 #ifdef SAV_3DGLCD
-  //#define U8GLIB_SSD1306
-  #define U8GLIB_SH1106
+  #define U8GLIB_SSD1306
+  //#define U8GLIB_SH1106
 #endif
   
 // Shift register panels
